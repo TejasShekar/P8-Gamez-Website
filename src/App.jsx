eb8bbde74635b911c6b4a998cf8603cc7d56d8e4
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-04-02 22:20:35 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Navbar } from "./components";
-import { HomePage, ProductListing, Cart, Wishlist, Login } from "./pages";
+import { HomePage, ProductListing, Cart, Wishlist, Login, SignUp } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductListing />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </div>
   );
