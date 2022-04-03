7b32e91187e5a42997894d39926ae8938452c0fa
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-04-03 22:47:08 +0530

    minor refactor & conditional rendering in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Header = () => {
+  const location = useLocation();
+
   return (
     <>
-      <header className="header flex-sbw-c flex-wrap">
-        <Link to="/" className="p-sm m-1">
-          <h1 className=" brand-name">P8 Gamez</h1>
-        </Link>
-        <label className="flex-center g-1">
-          <input
-            type="search"
-            id="search-box"
-            placeholder="Search for products"
-          />
-          <i className="fas fa-search search-btn"></i>
-        </label>
+      <header className="header flex-sbw-c flex-wrap pl-2">
+        {location.pathname === "/" && (
+          <>
+            <Link to="/products">
+              <h2>STORE</h2>
+            </Link>
+            <Link to="/" className="p-sm ml-3">
+              <h1 className="brand-name">P8 Gamez</h1>
+            </Link>
+          </>
+        )}
+        {location.pathname !== "/" && (
+          <>
+            <Link to="/" className="p-sm">
+              <h1 className="brand-name">P8 Gamez</h1>
+            </Link>
+            <label className="flex-center g-1">
+              <input
+                type="search"
+                id="search-box"
+                placeholder="Search for products"
+              />
+              <i className="fas fa-search search-btn"></i>
+            </label>
+          </>
+        )}
         <nav className="mx-1">
           <ul className="nav-list flex-sbw">
             <li className="mx-0 my-auto">
