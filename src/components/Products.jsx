26962af0fa85557fb754bf8e1572ccf82f930b86
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-04-03 23:00:54 +0530

    minor refactor in code

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,13 +6,13 @@ export const Products = () => {
   return (
     <main className="products">
       <div className="products-container grid g-2 mx-auto my-1">
-        {loader && <h2>loading...</h2>}
-        {productsList.map(({ _id, name, src, price, mrp, discount }) => {
+        {loader && <h2>L O A D I N G ...</h2>}
+        {productsList.map(({ _id, title, src, price, mrp, discount }) => {
           return (
             <div key={_id} className="card pos-relative flex-sbw-c flex-col">
-              <img className="card-img" src={src} alt={name} />
+              <img className="card-img" src={src} alt={title} />
               <div className="card-info flex-sbw flex-col">
-                <p className="card-title">{name}</p>
+                <p className="card-title">{title}</p>
                 <div className="price-info flex-center">
                   <div className="final-price">₹{price}</div>
                   <div className="mrp">₹{mrp}</div>
