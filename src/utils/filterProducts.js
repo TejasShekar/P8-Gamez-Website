4d93bac9cf1bfb351619e302776c0c11adc9e7da
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-04-05 22:15:46 +0530

    refactor: filter reducer function

--- src/utils/filterProducts.js
+++ src/utils/filterProducts.js
@@ -1,9 +1,8 @@
 export const filterProducts = (products, filterState) => {
   const { sortBy, category, price, rating } = filterState;
-  let filteredProducts = [...products];
 
   // filter by price b/w lowest and selected value
-  filteredProducts = products.filter((product) => product.price <= price);
+  let filteredProducts = products.filter((product) => product.price <= price);
 
   //   filter by rating higher than selected value
   filteredProducts = filteredProducts.filter(
@@ -12,21 +11,16 @@ export const filterProducts = (products, filterState) => {
 
   // sort by filters
   if (sortBy === "LOW_TO_HIGH") {
-    filteredProducts = [...filteredProducts].sort((a, b) => a.price - b.price);
+    filteredProducts = filteredProducts.sort((a, b) => a.price - b.price);
   }
   if (sortBy === "HIGH_TO_LOW") {
-    filteredProducts = [...filteredProducts].sort((a, b) => b.price - a.price);
+    filteredProducts = filteredProducts.sort((a, b) => b.price - a.price);
   }
 
   // filter by selected category
-  let finalProducts = filteredProducts.filter(
-    (product) => category[product.category]
-  );
 
-  //   conditional return of final array of products to be rendered.
-  if (finalProducts.length === 0) {
+  if (Object.values(category).every((checked) => !checked)) {
     return filteredProducts;
-  } else {
-    return finalProducts;
   }
+  return filteredProducts.filter((product) => category[product.category]);
 };
