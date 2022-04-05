c488907dbc12919607f7ba41f84c4f5ff921edb4
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-04-05 18:53:25 +0530

    refactor: stylesheets and cart
    - restructured and rearranged stylesheets folders and files
    - added a few classes for products styling
    - ran prettier on cart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,11 +5,11 @@ export const Cart = () => {
   useDocumentTitle("P8-Gamez | Cart");
   return (
     <>
-    <Header/>
-    <main className="flex-c flex-wrap m-1 g-1">
-      <CartItems />
-      <CartSummary />
-    </main>
+      <Header />
+      <main className="flex-c flex-wrap m-1 g-1">
+        <CartItems />
+        <CartSummary />
+      </main>
     </>
   );
 };
