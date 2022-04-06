f2176b2d5a54ea2844f8be4d46c055c6a1b29d84
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-04-06 20:23:52 +0530

    create reducer for cart management

---
+++ src/reducers/cartReducer.jsx
@@ -0,0 +1,35 @@
+export const cartReducer = (state, { type, payload }) => {
+  switch (type) {
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cartItems: [...state.cartItems, { ...payload, quantity: 1 }],
+      };
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((item) => item._id !== payload),
+      };
+    case "INCREMENT_ITEM":
+      return {
+        ...state,
+        cartItems: state.cartItems.map((item) =>
+          item._id === payload._id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ),
+      };
+    case "DECREMENT_ITEM":
+      return {
+        ...state,
+        cartItems: state.cartItems.map((item) =>
+          item._id === payload._id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
