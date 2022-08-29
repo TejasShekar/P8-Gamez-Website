41c9b4567b7cb8fa6bf0e60effaa95b707a1fb10
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-08-29 19:58:03 +0530

    refactor: removed context and header component

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,18 +1,12 @@
-import { Filters, Header, Products } from "../components";
+import { Filters, Products } from "../components";
 import { useDocumentTitle } from "../hooks/useDocumentTitle";
-import { ProductProvider } from "../context/product-context";
 
 export const ProductListing = () => {
   useDocumentTitle("P8-Gamez | Products");
   return (
-    <>
-      <Header />
-      <div className="product-wrapper">
-        <ProductProvider>
-          <Filters />
-          <Products />
-        </ProductProvider>
-      </div>
-    </>
+    <div className="product-wrapper">
+      <Filters />
+      <Products />
+    </div>
   );
 };
