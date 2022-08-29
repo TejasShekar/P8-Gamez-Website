3be3503add6e0369065e4a4a90da6f72ad951b7b
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-08-29 19:50:25 +0530

    refactor: removed unnecessary div

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -1,9 +1,5 @@
 import { useCart } from "../context/cart-context";
-import {
-  calcDiscount,
-  calcItemsPrice,
-  calcQuantity,
-} from "../utils/cartSummary";
+import { calcDiscount, calcItemsPrice, calcQuantity } from "../utils/cartSummary";
 
 export const CartSummary = () => {
   const {
@@ -15,26 +11,23 @@ export const CartSummary = () => {
   const totalQuantity = calcQuantity(cartItems);
 
   return (
-    <div>
-      <div className="cart-summary p-1">
-        {cartItems.length !== 0 && (
-          <>
-            <p className="cart-summary-title">CART SUMMARY ({totalQuantity})</p>
-            <div className="cart-item flex-sbw my-1 mx-0">
-              Items total <span className="span">₹ {itemsTotal}</span>
-            </div>
-            <div className="cart-item flex-sbw my-1 mx-0">
-              Discount <span className="span">- ₹ {discount}</span>
-            </div>
-            <hr />
-            <h3 className="cart-item flex-sbw-c my-1 mx-0">
-              ORDER TOTAL{" "}
-              <span className="span">₹ {itemsTotal - discount}</span>
-            </h3>
-            <button className="btn py-sm px-1 btn-primary">PLACE ORDER</button>
-          </>
-        )}
-      </div>
+    <div className="cart-summary p-1">
+      {cartItems.length !== 0 && (
+        <>
+          <p className="cart-summary-title">CART SUMMARY ({totalQuantity})</p>
+          <div className="cart-item flex-sbw my-1 mx-0">
+            Items total <span className="span">₹ {itemsTotal}</span>
+          </div>
+          <div className="cart-item flex-sbw my-1 mx-0">
+            Discount <span className="span">- ₹ {discount}</span>
+          </div>
+          <hr />
+          <h3 className="cart-item flex-sbw-c my-1 mx-0">
+            ORDER TOTAL <span className="span">₹ {itemsTotal - discount}</span>
+          </h3>
+          <button className="btn py-sm px-1 btn-primary">PLACE ORDER</button>
+        </>
+      )}
     </div>
   );
 };
