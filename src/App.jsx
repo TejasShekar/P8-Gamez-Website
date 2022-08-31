0316f9799ebad62b3bcb3cfcf7dd9710a8b7993d
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-08-31 14:19:42 +0530

    fix: header rendering and auth redirection
    Added `RequireAuth` component that handles redirects to previous page visited before authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { ToastContainer, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import { Header } from "./components";
+import { Header, RequireAuth } from "./components";
+import { useAuth } from "./context";
 import { HomePage, ProductListing, Cart, Wishlist, Login, SignUp } from "./pages";
 
 function App() {
   const { pathname } = useLocation();
+  const location = useLocation();
+  const previousRoute = location.state?.from?.pathname ?? "/products";
+  const {
+    authState: { isAuth },
+  } = useAuth();
+
   return (
     <div className="App">
       <ToastContainer
@@ -21,14 +28,26 @@ function App() {
         draggable
         pauseOnHover
       />
-      {pathname !== ("/login" || "/signup") && <Header />}
+      {pathname !== "/login" && pathname !== "/signup" && <Header />}
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductListing />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+        </Route>
+        {isAuth ? (
+          <>
+            <Route path="/login" element={<Navigate to={previousRoute} replace />} />
+            <Route path="/signup" element={<Navigate to={previousRoute} replace />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </>
+        )}
       </Routes>
     </div>
   );
