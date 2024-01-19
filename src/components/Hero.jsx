7a9c58721cd54c7a224b9afe3b0592b22041654b
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2024-01-19 17:06:07 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 export const Hero = () => {
   return (
     <div className="m-auto flex-center pos-relative">
-      <img className="img-responsive hero-img" src={heroImg} alt="Trending Games" />
+      <img
+        className="img-responsive hero-img"
+        src={heroImg}
+        alt="Trending Games"
+      />
       <div className="overlay pos-abs"></div>
       <button className="btn btn-primary py-sm px-1 pos-abs">
         <Link to="/products">Shop Games</Link>
