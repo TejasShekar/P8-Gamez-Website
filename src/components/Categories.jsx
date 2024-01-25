66fd19fe338db4ee39b84da7c1bcac6c01cc5357
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2024-01-25 11:10:32 +0530

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -23,14 +23,15 @@ export const Categories = () => {
   return (
     <>
       <h2 className="category-heading mt-1 text-center">C A T E G O R I E S</h2>
-      <div id="categories" className="flex-c flex-wrap g-1 m-1">
+      <div className="categories g-1 m-1">
         {categoryData.map(({ _id, categoryName, src, name }) => (
-          <div key={_id} className="category pos-relative">
+          <div
+            key={_id}
+            className="category pos-relative"
+            onClick={() => categoryRedirectHandler(categoryName)}
+          >
             <img className="category-img" src={src} alt={categoryName} />
-            <div
-              className="link category-text pos-abs flex flex-col p-1"
-              onClick={() => categoryRedirectHandler(categoryName)}
-            >
+            <div className="link category-text pos-abs flex flex-col p-1">
               <h2>{name}</h2>
             </div>
           </div>
